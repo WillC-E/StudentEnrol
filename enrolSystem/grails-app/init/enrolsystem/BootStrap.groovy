8a25a9a91dfec9b8d6fcd251d92b334e5c11fecc
willC-E <b5020855@my.shu.ac.uk>
2017-01-18 11:18:02 +0000

    created module class

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,8 +1,21 @@
-package enrolsystem
+package com.enrol
 
 class BootStrap {
 
     def init = { servletContext ->
+	def computing = new Course(
+
+		title:'BSc Hon Computing',
+		code:'COMP234',
+		leader:'DrMP',
+		department:'Computing',
+		startDate:new Date('11/11/2016'),
+		endDate:new Date('11/11/2020'),
+		numberOfStudents:45,
+		description:'''a good course''',
+		tuitionFees:900,
+		studyMode:'fullTime'
+		).save()
     }
     def destroy = {
     }
